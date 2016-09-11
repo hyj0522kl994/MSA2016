a6c06b386acb2095a0d5b34689f9ea0c90b364de
hyj0522kl994 <hyj0522kl994@outlook.com>
2016-09-12 00:43:24 +1200

    css and js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var myButton = document.getElementById("myBtn");
+
+    myButton.addEventListener("click", myFunction);
+
+    function myFunction(){
+
+        alert("Welcome to XYZ");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var button = document.getElementById("Btn");
+
+    button.addEventListener("click", function1);
+
+    function function1(){
+
+        alert("Turn off air cleaner -> open door , Successfully applied green solution!");
+    }
+});
